ac9b28795fd97bc1ed6e9924be24d23012f51d93
Fernanda Bighi <fernandabb909@gmail.com>
2025-07-30 14:09:44 -0300

    launch

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def read_root():
+    response = HTMLResponse(
+        content="<h1>Bem-vindo à Loja Virtual!</h1>",
+        status_code=200
+    )
+    return response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
