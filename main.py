56d22275191514984addb55ffa12fede512a3327
araujosmaria <claramaria7143@gmail.com>
2025-08-11 17:49:00 -0300

    login e cadastro prontos

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
-
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def read_root():
-    response = templates.TemplateResponse("index.html", {"request": {}})
-    return response
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/cadastro", response_class=HTMLResponse)
+async def cadastro_page(request: Request):
+    return templates.TemplateResponse("cadastro.html", {"request": request})
+
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
