d26f8eeda271d5aa7ba27d13001e9b179be1ecb3
araujosmaria <claramaria7143@gmail.com>
2025-08-25 16:42:23 -0300

    atualizado

--- routes/public_routes.py
+++ routes/public_routes.py
@@ -16,7 +16,19 @@ async def get_login(request: Request):
 async def get_cadastro(request: Request):
     return templates.TemplateResponse("cadastro.html", {"request": request})
 
+@router.get("/cadastro_cuidador")
+async def get_cadastro_cuidador(request: Request):
+    return templates.TemplateResponse("cadastro_cuidador.html", {"request": request})
 
-@router.get("/redefinir-senha")
-async def get_redefinir_senha(request: Request):
-    return templates.TemplateResponse("redefinir_senha.html", {"request": request})
+@router.get("/cadastro_contratante")
+async def get_cadastro_contratante(request: Request):
+    return templates.TemplateResponse("cadastro_contratante.html", {"request": request})
+
+
+@router.get("/redefinicao_senha")
+async def get_redefinicao_senha(request: Request):
+    return templates.TemplateResponse("redefinicao_senha.html", {"request": request})
+
+@router.get("/confirmar_redefinir_senha")
+async def get_confirmar_redefinir_senha(request: Request):
+    return templates.TemplateResponse("confirmar_redefinir_senha.html", {"request": request})
